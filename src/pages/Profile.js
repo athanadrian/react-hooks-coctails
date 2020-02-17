883f0527a0becfc3b3a495dea48b2ab44f3730a7
athanadrian <athanadrian@gmail.com>
2020-02-17 22:59:04 +0200

    fix profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,7 +5,7 @@ const Profile = () => {
     <section className="section profile-section">
       <h1 className="section-title">Profile</h1>
       <p>
-        A simple try to build an App that gives you infomation about any
+        A simple try to build an App that gives you information about any
         refreshing cocktail you wish to fix. In the near future it will include
         more features and functionality, <br />
         ....adding your cocktail, supporting more criteria for searching
