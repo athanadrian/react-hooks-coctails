6f2550e04d4fbb45c86900c3fc9b39e3e339145f
athanadrian <athanadrian@gmail.com>
2020-02-17 22:56:26 +0200

    updating profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,11 +5,12 @@ const Profile = () => {
     <section className="section profile-section">
       <h1 className="section-title">Profile</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
-        hic aspernatur error blanditiis?
+        A simple try to build an App that gives you infomation about any
+        refreshing cocktail you wish to fix. In the near future it will include
+        more features and functionality, <br />
+        ....adding your cocktail, supporting more criteria for searching
+        <br />
+        ....and m o r e!
       </p>
     </section>
   );
